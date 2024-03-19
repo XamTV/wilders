0a83a5d59d329ca7820bee221c4e13b962f151b4
Maxime MAUSSION <maximemaussion30@gmail.com>
2024-03-19 10:40:09 +0100

    added beautiful colors to my best copaings !

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("Nico J le GOAT DES POPUPS"));
+console.log(chalk.green("Nico G LE ROI DE LA PAGE D'ACCUEIL !"));
+console.log(chalk.red("Luryn, LA REINE DES KAHOOT ! "));
+console.log(chalk.cyan("Amel, AMATERASTSUUUUUUUUUU "));
